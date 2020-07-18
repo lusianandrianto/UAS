6b46615f2b9adc24f1a5b9d957c417ad495a570d
lusianandrianto <61747649+lusianandrianto@users.noreply.github.com>
2020-07-18 14:27:12 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+$koneksi = mysqli_connect("localhost","id14354077_admin","Andri20.anto",'id14354077_web2');
+ 
+
+if (mysqli_connect_errno()){
+	echo 'Koneksi database gagal : ' . mysqli_connect_error();
+}
+?>
